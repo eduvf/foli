e4a0dff91654c90b6cec9074dffb44e1d1627458
eduvf <155389249+eduvf@users.noreply.github.com>
2024-02-25 18:56:18 +0100

    add scan dir

--- pineapage.go
+++ pineapage.go
@@ -3,12 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	dir := flag.String("d", ".", "directory to work on")
 	flag.Parse()
 
-	fmt.Println("pineapage")
-	fmt.Println(string(*dir))
+	err := filepath.WalkDir(*dir, scan)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func scan(path string, file fs.DirEntry, err error) error {
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	if strings.HasPrefix(path, ".") {
+		return nil
+	}
+	fmt.Printf("%v %s\n", file.IsDir(), path)
+	return nil
 }
