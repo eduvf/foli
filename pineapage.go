98d9966854180826b875c50711072599db7390d9
eduvf <155389249+eduvf@users.noreply.github.com>
2024-03-17 21:54:14 +0100

    simple server

--- pineapage.go
+++ pineapage.go
@@ -4,9 +4,16 @@ import (
 	"flag"
 	"fmt"
 	"io/fs"
+	"log"
+	"net/http"
 	"path/filepath"
 	"strings"
 )
+
+type Page struct {
+	Title string
+	Body  string
+}
 
 func main() {
 	dir := flag.String("d", ".", "directory to work on")
@@ -16,6 +23,9 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	http.HandleFunc("/", view)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func scan(path string, file fs.DirEntry, err error) error {
@@ -28,4 +38,12 @@ func scan(path string, file fs.DirEntry, err error) error {
 	}
 	fmt.Printf("%v %s\n", file.IsDir(), path)
 	return nil
+}
+
+func view(writer http.ResponseWriter, request *http.Request) {
+	p := &Page{
+		Title: "Test",
+		Body:  "Hi!",
+	}
+	fmt.Fprintf(writer, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
