33379f663575de888a76992f0fbc0d6856b8ac88
eduvf <155389249+eduvf@users.noreply.github.com>
2024-03-27 16:52:18 +0100

    single pass parser

--- pineapage.go
+++ pineapage.go
@@ -1,29 +1,61 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
 
 var (
-	h3     = regexp.MustCompile(`(?m)^### (.*)`)
-	h2     = regexp.MustCompile(`(?m)^## (.*)`)
-	h1     = regexp.MustCompile(`(?m)^# (.*)`)
-	pre    = regexp.MustCompile("(?s)```(.*)```")
-	quote  = regexp.MustCompile(`(?m)^> (.*)`)
-	anchor = regexp.MustCompile(`\[(.*)\]\((.*)\)`)
+	h3     = regexp.MustCompile(`^### (.*)`)
+	h2     = regexp.MustCompile(`^## (.*)`)
+	h1     = regexp.MustCompile(`^# (.*)`)
+	quote  = regexp.MustCompile(`^> (.*)`)
+	anchor = regexp.MustCompile(`^\[(.*)\]\((.*)\)`)
 )
 
 func parse(s string) string {
-	s = h3.ReplaceAllString(s, `<h3>$1</h3>`)
-	s = h2.ReplaceAllString(s, `<h2>$1</h2>`)
-	s = h1.ReplaceAllString(s, `<h1>$1</h1>`)
-	s = pre.ReplaceAllString(s, `<pre>$1</pre>`)
-	s = quote.ReplaceAllString(s, `<blockquote>$1</blockquote>`)
-	s = anchor.ReplaceAllString(s, `<a href="$2">$1</a>`)
-	return s
+	result := ""
+	isPre := false
+	scanner := bufio.NewScanner(strings.NewReader(s))
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if isPre {
+			if line == "```" {
+				isPre = false
+				result += "</pre>\n"
+			} else {
+				result += line + "\n"
+			}
+			continue
+		}
+
+		switch {
+		case h3.MatchString(line):
+			result += `<h3>` + line[4:] + `</h3>`
+		case h2.MatchString(line):
+			result += `<h2>` + line[3:] + `</h2>`
+		case h1.MatchString(line):
+			result += `<h1>` + line[2:] + `</h1>`
+		case quote.MatchString(line):
+			result += `<blockquote>` + line[2:] + `</blockquote>`
+		case anchor.MatchString(line):
+			result += anchor.ReplaceAllString(line, `<a href="$2">$1</a>`)
+		case line == "```":
+			result += `<pre>`
+			isPre = true
+		default:
+			result += line
+		}
+
+		result += "\n"
+	}
+
+	return result
 }
 
 func page(w http.ResponseWriter, r *http.Request) {
